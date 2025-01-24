b232cd0f84a323a4a8c91eded4d53adda0470cb0
Nguyễn Thanh Tú <nguyenthanhtu@vnpt.vn>
2025-01-24 14:44:29 +0700

    khoitao

---
+++ IntegrationInvoice.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntegrationInvoice.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
