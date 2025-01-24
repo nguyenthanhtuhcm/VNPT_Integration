a4d30c4f71b528db93e2a176b3a30a5e1bbff5b2
Nguyễn Thanh Tú <nguyenthanhtu@vnpt.vn>
2025-01-24 22:36:58 +0700

    a

---
+++ IntegrationInvoice.Api/MigrationManager.cs
@@ -0,0 +1,22 @@
+﻿using IntegrationInvoice.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegrationInvoice.Api
+{
+    public static class MigrationManager
+    {
+        public static WebApplication MigrateDatabase(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                using (var context = scope.ServiceProvider.GetRequiredService<IntegrationInvoiceContext>())
+                {
+                    context.Database.Migrate();
+                    new DataSeeder().SeedAsync(context).Wait();
+                }
+            }
+            return app;
+        }
+    }
+
+}
---
+++ IntegrationInvoice.Data/IntegrationInvoiceContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationInvoice.Data
+{
+    
+    public class IntegrationInvoiceContextFactory : IDesignTimeDbContextFactory<IntegrationInvoiceContext>
+    {
+        public IntegrationInvoiceContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<IntegrationInvoiceContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new IntegrationInvoiceContext(builder.Options);
+        }
+    }
+}
